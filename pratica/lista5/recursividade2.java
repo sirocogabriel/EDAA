e69bc212de493dd87b7a1dd59671f6942f299de2
Gabriel Siroco <sirocogabriel@gmail.com>
2022-10-09 15:56:41 -0300

    resursividade 2 e 3

--- pratica/lista5/recursividade2.java
+++ pratica/lista5/recursividade2.java
@@ -7,15 +7,18 @@ public class recursividade2 {
         try (Scanner s = new Scanner(System.in)) {
             int num = s.nextInt();
 
+            System.out.println("\nNumeros inteiros de 0 até o número inserido:");
             PrintRecursivo(num, 0);
         }
     }
 
     public static int PrintRecursivo (int num, int numPrint) {
         if(numPrint == num) {
-            return numPrint;
+            System.out.println(numPrint);
+            return 0;
         }
         else {
+            System.out.println(numPrint);
             numPrint++;
             return PrintRecursivo(num, numPrint);
         }
--- pratica/lista5/recursividade3.java
+++ pratica/lista5/recursividade3.java
@@ -1,98 +1,28 @@
 package pratica.lista5;
-import java.util.*;
+import java.util.Scanner;
 
-class recursividade3 {
-    
+public class recursividade3 {
     public static void main(String[] args) {
-        int op = 0;
-        try (Scanner s = new Scanner(System.in)) {
-            ArrayList<Carro> carros = new ArrayList<Carro>();
-            
-            while(op < 1000){
-                
-                System.out.println("\nDigite o número da opção desejada: \n");
-                System.out.println("(1) Adicionar um carro");
-                System.out.println("(2) Mostrar carro mais econômico");
-                System.out.println("(3) Mostrar quantos litros cada carro consome para uma determinada distância");
-                System.out.println("(4) Sair\n");
-                op = s.nextInt();
-                
-                if(op == 1){
-                    carros = AdicionaCarro(carros);
-                }else if(op == 2){
-                    System.out.println("\n"+MaisEconomico(carros));
-                }else if(op == 3){
-                    ConsumoPorDistancia(carros);
-                }else if(op == 4){
-                    op = 1000;
-                }
-            }
-        }
-    }
-    
-    static ArrayList<Carro> AdicionaCarro (ArrayList<Carro> carros) {
+        Scanner s = new Scanner(System.in);
         
-        try (Scanner s = new Scanner(System.in)) {
-            Carro carro = new Carro();
-            boolean tudoCerto = true;
-                
-            System.out.print("\nDigite o nome do "+(carros.size() + 1)+"º carro: ");
-            carro.Nome = s.nextLine();
-   
-            for (Carro c : carros) {
-                tudoCerto = c.ChecaNome(carro.Nome);
-            }
+        System.out.println("Qual o k?");
+        int k = s.nextInt();
 
-            if(!tudoCerto) return carros;
+        System.out.println("\nQual o n?");
+        int n = s.nextInt();
 
-            System.out.print("Digite quantos quilômetros por litro o "+(carros.size() + 1)+"º carro faz: ");
-            carro.KmPorLitro = s.nextFloat();
-   
-            carros.add(carro);
-        }
-        return carros;
+        System.out.println("\nResultado: " + ExponencialRecursivo(k, n, 0, 1));
+        s.close();
     }
 
-    static String MaisEconomico (ArrayList<Carro> carros) {
-
-        String maisEconomicoNome = "";
-        Float maisEconomicoKmPorLitro = Float.parseFloat("-1");
-        
-
-        for (Carro carro : carros) {
-            if(maisEconomicoKmPorLitro < carro.KmPorLitro){
-                maisEconomicoNome = carro.Nome;
-                maisEconomicoKmPorLitro = carro.KmPorLitro;
-            }
+    public static int ExponencialRecursivo (int k, int n, int count, int total) {
+        if(count == n) {
+            return total;
         }
-        return maisEconomicoNome;
-    }
-
-    static void ConsumoPorDistancia (ArrayList<Carro> carros) {
-        System.out.print("\nDigite a distância percorrida: ");
-        try (Scanner s = new Scanner(System.in)) {
-            float dist = s.nextFloat();
-
-            System.out.print("\nLitros gastos: ");
-
-            for (Carro carro : carros) {
-                System.out.print("\n" + carro.Nome + ": " + (dist/carro.KmPorLitro));
-            }
+        else {
+            total = k * total;
+            count++;
+            return ExponencialRecursivo(k, n, count, total);
         }
-        System.out.print("\n");
-    }
-    
-}
-
-class Carro {
-    String Nome;
-    Float KmPorLitro;
-
-    Boolean ChecaNome(String nomeCarro){
-        if(Nome == nomeCarro){
-            System.out.println("\nJá existe um carro com esse nome!\n");
-            return false;
-        }
-        return true;
     }
 }
