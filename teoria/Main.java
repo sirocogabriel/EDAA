ace4443d18d23e8e99dbb0aa1ca385eb2b8d11e8
Gabriel Siroco <sirocogabriel@gmail.com>
2022-10-25 22:03:41 -0300

    ajuste

--- teoria/Main.java
+++ teoria/Main.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int[] A = new int[10000];
+        int[] AInvertido = new int[10000];
         Random r = new Random();
         int low = 10;
         int high = 10000000;
@@ -20,18 +21,59 @@ public class Main {
         is = new InsertionSort(A.clone());
         ms = new MergeSort(A.clone());
         bs = new BubbleSort(A.clone());
+        long timeIs = 0;
+        long timeMs = 0;
+        long timeBs = 0;
         long startTime = System.currentTimeMillis();
         is.sort();
-        System.out.println("Insertion sort time: " + (System.currentTimeMillis() - startTime));
+        timeIs = (System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        ms.sort(0, (A.length - 1));
+        timeMs += (System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        bs.sort();
+        timeBs += (System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        is.sort();
+        timeIs += (System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        ms.sort(0, (A.length - 1));
+        timeMs += (System.currentTimeMillis() - startTime);
+
+        startTime = System.currentTimeMillis();
+        bs.sort();
+        timeBs += (System.currentTimeMillis() - startTime);
+
+        A = is.returnSort();
+
+        for (int i = 0; i < 10000; i++){
+            AInvertido[9999 - i] = A[i];
+        }
+
+        is = new InsertionSort(AInvertido.clone());
+        ms = new MergeSort(AInvertido.clone());
+        bs = new BubbleSort(AInvertido.clone());
+
+        System.out.println();
+        startTime = System.currentTimeMillis();
+        is.sort();
+        timeIs += (System.currentTimeMillis() - startTime);
+        System.out.println("Insertion sort time: " + timeIs);
 
         System.out.println();
         startTime = System.currentTimeMillis();
         ms.sort(0, (A.length - 1));
-        System.out.println("Mergesort time: " + (System.currentTimeMillis() - startTime));
+        timeMs += (System.currentTimeMillis() - startTime);
+        System.out.println("Mergesort time: " + timeMs);
 
         System.out.println();
         startTime = System.currentTimeMillis();
         bs.sort();
-        System.out.println("BubbleSort time: " + (System.currentTimeMillis() - startTime));
+        timeBs += (System.currentTimeMillis() - startTime);
+        System.out.println("BubbleSort time: " + timeBs);
     }
 }
--- teoria/ordenacao/InsertionSort.java
+++ teoria/ordenacao/InsertionSort.java
@@ -23,4 +23,18 @@ public class InsertionSort {
             vector[i+1] = key;
         }
     }
+
+    public int[] returnSort(){
+        int key;
+        for (int j = 1; j < vector.length; j++) {
+            key = vector[j];
+            int i = j-1;
+            while ( (i > 0) && (vector[i]>key)){
+                vector[i+1] = vector[i];
+                i--;
+            }
+            vector[i+1] = key;
+        }
+        return vector;
+    }
 }
